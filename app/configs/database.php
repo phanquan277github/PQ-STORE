142ae717403f5568993e42fbe78ca2247a0a8c50
phanquan277 <phanquan277@ubuntu.localdomain>
2025-09-19 00:00:40 +0700

    Cau hinh docker trien khai tren ubuntu

--- app/configs/database.php
+++ app/configs/database.php
@@ -1,8 +1,8 @@
 <?php
 $config['database'] = [
-  'host' => '127.0.0.1',
+  'host' => 'db',
   'port' => '3306',
-  'user' => 'root',
-  'pass' => 'phanquan277',
-  'dbname' => 'pq_store_test'
+  'user' => 'pquser',
+  'pass' => 'pqpass',
+  'dbname' => 'pqstore'
 ];
--- data/init_database.sh
+++ data/init_database.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+
+MARKER="/var/lib/mysql/.db_inited"
+
+if [ -f "$MARKER" ]; then
+  echo "⚠️ Init script đã chạy trước đó, bỏ qua."
+  exit 0
+fi
+
+echo "🚀 Bắt đầu khởi tạo database ..."
+
 # Config DB (lấy từ biến môi trường docker-compose)
 DB_HOST="localhost"
 DB_USER="${MYSQL_USER:-pquser}"
@@ -15,13 +25,10 @@ run_sql() {
 
 echo "🚀 Bắt đầu khởi tạo database $DB_NAME ..."
 
-# 1. Tạo bảng
-run_sql "/docker-entrypoint-initdb.d/CreateTable.sql"
-
-# 2. Insert banners & slideshow
+# 1. Insert banners & slideshow
 run_sql "/docker-entrypoint-initdb.d/InsertData/insertBannersAndSlideshow.sql"
 
-# 3. Insert categories theo từng cụm
+# 2. Insert categories theo từng cụm
 echo "==> Insert Categories (root + bậc 1)"
 awk '/-- root cate/,/-- bac 1/' /docker-entrypoint-initdb.d/InsertData/insertCategories.sql \
     | mysql -h $DB_HOST -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
@@ -37,10 +44,12 @@ echo "==> Insert Categories (bậc 3)"
 awk '/-- bac 3/,0' /docker-entrypoint-initdb.d/InsertData/insertCategories.sql \
     | mysql -h $DB_HOST -u"$DB_USER" -p"$DB_PASS" "$DB_NAME"
 
-# 4. Insert products
+# 3. Insert products
 run_sql "/docker-entrypoint-initdb.d/InsertData/insertProducts.sql"
 
-# 5. Insert phụ
-run_sql "/docker-entrypoint-initdb.d/InsertData/inserPhu.sql"
+# 4. Insert phụ
+run_sql "/docker-entrypoint-initdb.d/InsertData/insertPhu.sql"
 
 echo "✅ Database $DB_NAME đã được khởi tạo thành công!"
+
+touch "$MARKER"
